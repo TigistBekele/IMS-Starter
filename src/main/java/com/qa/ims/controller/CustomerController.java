d7bd02073cbe343384429dc910cf419825420316
TigistBekele <ttbtigist2020@gmail.com>
2021-05-28 10:27:51 -0500

    TB: initial commit

--- src/main/java/com/qa/ims/controller/CustomerController.java
+++ src/main/java/com/qa/ims/controller/CustomerController.java
@@ -34,7 +34,7 @@ public class CustomerController implements CrudController<Customer> {
 		List<Customer> customers = customerDAO.readAll();
 		for (Customer customer : customers) {
 			LOGGER.info(customer);
-		}
+		}  
 		return customers;
 	}
 
